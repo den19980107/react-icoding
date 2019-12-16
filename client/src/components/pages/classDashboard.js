e651942ebf7ab3924b8482af86f6ff0ef5206a0a
Paul <den19980107@gmail.com>
2019-12-16 10:05:27 +0800

    新增錯誤頁面

--- client/src/components/pages/classDashboard.js
+++ client/src/components/pages/classDashboard.js
@@ -9,6 +9,7 @@ import StudySituation from '../courses/studySituation';
 import VideoSituation from '../courses/videoSituation';
 import Article from '../courses/article';
 import QRcode from '../courses/qrcode';
+import Error from '../pages/error';
 
 
 import axios from 'axios';
@@ -68,90 +69,96 @@ class classDashboard extends Component {
    }
 
    render() {
-      return (
-         <Layout style={{ minHeight: '100vh' }}>
-            <Sider collapsible collapsed={this.state.collapsed} onCollapse={this.onCollapse}>
-               <div className="logo" />
-               <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" onClick={this.changePanel}>
-                  <Menu.Item key="classInfo">
-                     <Icon type="info-circle" />
-                     <span>課程資訊</span>
-                  </Menu.Item>
-                  <SubMenu
-                     key="unit"
-                     title={
-                        <span>
-                           <Icon type="file" />
-                           <span>單元列表</span>
-                        </span>
-                     }
-                  >
-                     {this.state.units.map(unit => (
-                        <Menu.Item key={unit._id}>{unit.unitName}</Menu.Item>
-                     ))}
+      if (this.state.classId) {
+         return (
+            <Layout style={{ minHeight: '100vh' }}>
+               <Sider collapsible collapsed={this.state.collapsed} onCollapse={this.onCollapse}>
+                  <div className="logo" />
+                  <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" onClick={this.changePanel}>
+                     <Menu.Item key="classInfo">
+                        <Icon type="info-circle" />
+                        <span>課程資訊</span>
+                     </Menu.Item>
+                     <SubMenu
+                        key="unit"
+                        title={
+                           <span>
+                              <Icon type="file" />
+                              <span>單元列表</span>
+                           </span>
+                        }
+                     >
+                        {this.state.units.map(unit => (
+                           <Menu.Item key={unit._id}>{unit.unitName}</Menu.Item>
+                        ))}
 
-                  </SubMenu>
-                  <Menu.Item key="assistant">
-                     <Icon type="user" />
-                     <span>助教管理</span>
-                  </Menu.Item>
-                  <Menu.Item key="classMate">
-                     <Icon type="team" />
-                     <span>本班成員</span>
-                  </Menu.Item>
-                  <Menu.Item key="reviewTest">
-                     <Icon type="file-done" />
-                     <span>批閱測驗</span>
-                  </Menu.Item>
-                  <Menu.Item key="studySituation">
-                     <Icon type="pie-chart" />
-                     <span>學習狀況</span>
-                  </Menu.Item>
-                  <Menu.Item key="vidoeSituation">
-                     <Icon type="bar-chart" />
-                     <span>影片觀看情形</span>
-                  </Menu.Item>
-                  <Menu.Item key="article">
-                     <Icon type="sound" />
-                     <span>討論區</span>
-                  </Menu.Item>
-                  <Menu.Item key="qrcode">
-                     <Icon type="qrcode" />
-                     <span>QR CODE</span>
-                  </Menu.Item>
-               </Menu>
-            </Sider>
-            <Layout>
-               {this.state.nowPanel.pannelId == "classInfo" &&
-                  <ClassInfo id={this.state.classId}></ClassInfo>
-               }
-               {this.state.nowPanel.pannelId == "unit" &&
-                  <Unit unit={this.getUnit()}></Unit>
-               }
-               {this.state.nowPanel.pannelId == "assistant" &&
-                  <Assistant></Assistant>
-               }
-               {this.state.nowPanel.pannelId == "classMate" &&
-                  <ClassMate classId={this.state.classId}></ClassMate>
-               }
-               {this.state.nowPanel.pannelId == "reviewTest" &&
-                  <ReviewTest></ReviewTest>
-               }
-               {this.state.nowPanel.pannelId == "studySituation" &&
-                  <StudySituation></StudySituation>
-               }
-               {this.state.nowPanel.pannelId == "vidoeSituation" &&
-                  <VideoSituation></VideoSituation>
-               }
-               {this.state.nowPanel.pannelId == "article" &&
-                  <Article></Article>
-               }
-               {this.state.nowPanel.pannelId == "qrcode" &&
-                  <QRcode></QRcode>
-               }
-            </Layout>
-         </Layout >
-      );
+                     </SubMenu>
+                     <Menu.Item key="assistant">
+                        <Icon type="user" />
+                        <span>助教管理</span>
+                     </Menu.Item>
+                     <Menu.Item key="classMate">
+                        <Icon type="team" />
+                        <span>本班成員</span>
+                     </Menu.Item>
+                     <Menu.Item key="reviewTest">
+                        <Icon type="file-done" />
+                        <span>批閱測驗</span>
+                     </Menu.Item>
+                     <Menu.Item key="studySituation">
+                        <Icon type="pie-chart" />
+                        <span>學習狀況</span>
+                     </Menu.Item>
+                     <Menu.Item key="vidoeSituation">
+                        <Icon type="bar-chart" />
+                        <span>影片觀看情形</span>
+                     </Menu.Item>
+                     <Menu.Item key="article">
+                        <Icon type="sound" />
+                        <span>討論區</span>
+                     </Menu.Item>
+                     <Menu.Item key="qrcode">
+                        <Icon type="qrcode" />
+                        <span>QR CODE</span>
+                     </Menu.Item>
+                  </Menu>
+               </Sider>
+               <Layout>
+                  {this.state.nowPanel.pannelId == "classInfo" &&
+                     <ClassInfo id={this.state.classId}></ClassInfo>
+                  }
+                  {this.state.nowPanel.pannelId == "unit" &&
+                     <Unit unit={this.getUnit()}></Unit>
+                  }
+                  {this.state.nowPanel.pannelId == "assistant" &&
+                     <Assistant></Assistant>
+                  }
+                  {this.state.nowPanel.pannelId == "classMate" &&
+                     <ClassMate classId={this.state.classId}></ClassMate>
+                  }
+                  {this.state.nowPanel.pannelId == "reviewTest" &&
+                     <ReviewTest></ReviewTest>
+                  }
+                  {this.state.nowPanel.pannelId == "studySituation" &&
+                     <StudySituation></StudySituation>
+                  }
+                  {this.state.nowPanel.pannelId == "vidoeSituation" &&
+                     <VideoSituation></VideoSituation>
+                  }
+                  {this.state.nowPanel.pannelId == "article" &&
+                     <Article></Article>
+                  }
+                  {this.state.nowPanel.pannelId == "qrcode" &&
+                     <QRcode></QRcode>
+                  }
+               </Layout>
+            </Layout >
+         );
+      } else {
+         return (
+            <Error></Error>
+         )
+      }
    }
 }
 
---
+++ client/src/components/pages/error.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import '../style/errorPage.css';
+class error extends Component {
+   render() {
+      return (
+         <div style={{ height: "100vh", width: "100vw", background: "#000" }}>
+            <figure>
+               <div class="sad-mac"></div>
+               <figcaption>
+                  <span class="sr-text">Error 404: Not Found</span>
+                  <span class="e"></span>
+                  <span class="r"></span>
+                  <span class="r"></span>
+                  <span class="o"></span>
+                  <span class="r"></span>
+                  <span class="_4"></span>
+                  <span class="_0"></span>
+                  <span class="_4"></span>
+                  <span class="n"></span>
+                  <span class="o"></span>
+                  <span class="t"></span>
+                  <span class="f"></span>
+                  <span class="o"></span>
+                  <span class="u"></span>
+                  <span class="n"></span>
+                  <span class="d"></span>
+               </figcaption>
+            </figure>
+         </div>
+      );
+   }
+}
+
+export default error;
